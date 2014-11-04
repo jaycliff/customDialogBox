e4d32cfab27664be39c2d30d54e4096e3048f5ee
Cliffmeister <jaycliff@users.noreply.github.com>
2014-11-04 16:52:16 +0800

    Update custom_dialogue_box.js

--- custom_dialogue_box.js
+++ custom_dialogue_box.js
@@ -1,5 +1,5 @@
 /*
-    Copyright 2014 Jaycliff Arcilla
+    Copyright 2014 Jaycliff Arcilla of Eversun Software Philippines Corporation (Davao Branch)
     
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
